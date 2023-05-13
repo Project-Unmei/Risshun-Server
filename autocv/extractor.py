76d87e3f38c3468ecd1eb905d744bbf148d0f3fe
TheLeoChai <43290645+TheLeoChai@users.noreply.github.com>
2023-05-13 05:54:03 -0400

    added nlp implementation for text extraction
    nlp text extraction without keyword set. Not tested

--- autocv/extractor.py
+++ autocv/extractor.py
@@ -6,6 +6,23 @@ try:
     import nltk
     
     from rake_nltk import Rake
+
+    # extract_text_with_nlp dependency
+    import numpy as np
+    import pandas as pd
+    import string
+    from nlk.corpus import stopwords
+    from nltk.naive_bayes import MultinomialNB
+    from sklearn.feature_extraction.text import TFidfVectorizer
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+    from wordcloud import WordCloud
+    import matplotlib.pyplot as plt
+    %matplotlib inline
+    from textblob import Word
+    from textblob import TextBlob
+
+
 finally:
     def extract_text_with_rake(para: str):
         rakeInstance = Rake()
@@ -13,12 +30,116 @@ finally:
         tempList = rakeInstance.get_ranked_phrases()
         return tempList
 
-def extract_text_with_spacy(para: str):
-    # Input: Just a paragraph
-    """"""
-    doc = []
-    # Return a list of words
-    return doc
+debug = True
+
+# Uses local machine to power nlp text extraction
+# input: 
+#       job: string of job title
+#       para: string of text to be analyzed
+# output:
+#       lst: list of keywords extracted from text sorted by importance
+def extract_text_with_nlp(job: str, para: str):
+
+    ##DATAFRAME CREATION
+    d = {'job': [job], 'description': [para]}
+    df = pd.DataFrame(data=d)
+        
+    if debug:
+        print(df.head())
+        print(df.shape)
+
+    ##PREPROCESSING
+    # lower case
+    df['description'] = df['description'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+    # remove punctuation
+    df['description'] = df['description'].str.replace('[^\w\s]','')
+    # remove digits
+    df['description'] = df['description'].str.replace('\d','')
+    # remove stopwords
+    stop = stopwords.words('english')
+    df['description'] = df['description'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+    other_stop = ['junior', 'senior','experience','etc','job','work','company','technique',
+                        'candidate','skill','skills','language','menu','inc','new','plus','years',
+                        'technology','organization','ceo','cto','account','manager','data','scientist','mobile',
+                        'developer','product','revenue','strong']
+    df['description'] = df['description'].apply(lambda x: " ".join(x for x in x.split() if x not in other_stop))
+    # lemmatization
+    df['description'] = df['description'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+    
+    # good programming languages would not require the above commments about what the code does
+    # the code would be self explanitory
+    # proof that eithe I am dogshit at python or python is dogshit
+
+    if debug:
+        print(df.head())
 
 
+    ##MODELING
+    vectorize = TfidfVectorizer()
 
+    x = vectorize.fit_transform(df.description)
+    y = df.job
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=109)
+
+    clf = MultinomialNB()
+    clf.fit(x_train, y_train)
+    y.predict = clf.predict(x_test)
+
+    if debug:
+        y_test.hist()
+        y_train.hist()
+
+    ##EVALUATION
+    if debug:
+        print("score: ", accuracy_score(y_test, y.predict))
+        print("classes: ", classification_report(y_test, y.predict))
+
+    ##FEATURE EXTRACTION
+    # list of technical skills may need expansion
+    technical = ['python', 'c','r', 'c++','java','hadoop','scala','flask','pandas','spark','scikit-learn',
+                    'numpy','php','sql','mysql','css','mongdb','nltk','fastai' , 'keras', 'pytorch','tensorflow',
+                   'linux','Ruby','JavaScript','django','react','reactjs','ai','ui','tableau']
+
+    feature_array = vectorizer.get_feature_names()
+    features = len(feature_array)
+    n_max = int(features * 0.1)
+
+    output = pd.DataFrame()
+    for i in range(0,len(clf.classes_)):
+         print("\n****" ,clf.classes_[i],"****\n")
+        class_prob_indices_sorted = clf.feature_log_prob_[i, :].argsort()[::-1]
+        raw_skills = np.take(feature_array, class_prob_indices_sorted[:n_max])
+        print("list of unprocessed skills :")
+        print(raw_skills)
+        
+        ## Extract technical skills
+        top_technical_skills= list(set(technical_skills).intersection(raw_skills))[:6]
+        #print("Top technical skills",top_technical_skills)
+        
+        ## Extract adjectives
+        
+        # Delete technical skills from raw skills list
+        ## At this steps, raw skills list doesnt contain the technical skills
+        #raw_skills = [x for x in raw_skills if x not in top_technical_skills]
+        #raw_skills = list(set(raw_skills) - set(top_technical_skills))
+
+        # transform list to string
+        txt = " ".join(raw_skills)
+        blob = TextBlob(txt)
+        #top 6 adjective
+        top_adjectives = [w for (w, pos) in TextBlob(txt).pos_tags if pos.startswith("JJ")][:6]
+        #print("Top 6 adjectives: ",top_adjectives)
+        
+        output = output.append({'job_title':clf.classes_[i],
+                            'technical_skills':top_technical_skills,
+                            'soft_skills':top_adjectives },
+                        ignore_index=True)
+
+        print(output.T)
+
+    ##OUTPUT
+    output_array = output.to_numpy()
+    output_list = output_array.tolist()
+    return output_list
+    
