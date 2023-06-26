f93e2d2f44fb0839f84646597c39ceecd713f520
Tim Yang <thryang@outlook.com>
2023-06-26 06:28:33 -0400

    Further Prompt Tuning
    Tuned prompt for ensuring generation only has skills contained within resume.

--- autocv/extractor.py
+++ autocv/extractor.py
@@ -51,10 +51,20 @@ finally:
                 {"role": "user", 
                  "content": f"""Taking into account this job description and my resume, generate {para_count} distinctly 
                                 named skills (preferably soft skills) and write me a cover letter paragraphs for each 
-                                skill, each of around 80 words. Note that all three paragraphs will eventually be in one 
-                                cover letter, so avoid repetition among paragraphs, assume paragraphs will be in order. 
-                                The tone of the paragraphs will be formal, and avoid repeating traits already mentioned 
-                                in the resume. Respond text should be formatted like a json, where key is the name of 
+                                skill, each of around 80 words. 
+                                
+                                Note that all three paragraphs will eventually be in one 
+                                cover letter, so avoid repetition among paragraphs, assume paragraphs will be in order ,
+                                and try to make the paragraphs flow well together, so dont make it seem like dot jots. 
+                                Incorporate some phrases or names from the job description and resume into the 
+                                paragraphs if possible, but do not force it. 
+                                
+                                The tone of the paragraphs will be formal, but not too serious, and avoid repeating 
+                                traits already mentioned in the resume, you can make simple implies from the resume. 
+                                Make sure that the paragraphs are not too similar to each other. Try to keep the 
+                                beginning of each paragraph unique, and try to make the paragraphs flow well together.
+                                
+                                Respond text should be formatted like a json, where key is the name of 
                                 the skill without enumeration or the word skill, and value is the paragraph. DO NOT MAKE
                                 UP SKILLS THAT ARE NOT IN MY RESUME."
                                 """}
